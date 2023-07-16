534248a8d5c7caaba797be1e2310e3447b4ade12
NazarStf <hworker98@gmail.com>
2023-07-17 00:34:13 +0300

    Lesson 2 - Create ApexPredator Model

---
+++ JP Apex Predators/ApexPredator.swift
@@ -0,0 +1,23 @@
+//
+//  ApexPredator.swift
+//  JP Apex Predators
+//
+//  Created by NazarStf on 16.07.2023.
+//
+
+import Foundation
+
+struct ApexPredator: Codable {
+	let id: Int
+	let name: String
+	let type: String
+	let movies: [String]
+	let movieScenes: [MovieScene]
+	let link: String
+}
+
+struct MovieScene: Codable {
+	let id: Int
+	let movie: String
+	let sceneDescription: String
+}
