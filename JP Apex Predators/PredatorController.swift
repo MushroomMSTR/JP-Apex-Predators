59707c84e504f582064ad4e0be6a2d30dac171ba
NazarStf <hworker98@gmail.com>
2023-07-17 14:21:48 +0300

    Lesson 3 - Create ApexPredator Controller

---
+++ JP Apex Predators/PredatorController.swift
@@ -0,0 +1,29 @@
+//
+//  PredatorController.swift
+//  JP Apex Predators
+//
+//  Created by NazarStf on 17.07.2023.
+//
+
+import Foundation
+
+class PredatorController {
+	var apexPredators: [ApexPredator] = []
+	
+	init () {
+		decodeApexPredatorData()
+	}
+	
+	func decodeApexPredatorData() {
+		if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
+			do {
+				let data = try Data(contentsOf: url)
+				let decoder = JSONDecoder()
+				decoder.keyDecodingStrategy = .convertFromSnakeCase
+				apexPredators = try decoder.decode([ApexPredator].self, from: data)
+			} catch {
+				print("Error decoding JSON data: \(error)")
+			}
+		}
+	}
+}
