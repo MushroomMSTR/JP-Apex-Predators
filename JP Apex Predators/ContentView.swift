a4b09420ed775037e87f98a5283f539362867213
NazarStf <hworker98@gmail.com>
2023-07-17 19:41:56 +0300

    Lesson 11 - Menus, Pickers, & Animations

--- JP Apex Predators/ContentView.swift
+++ JP Apex Predators/ContentView.swift
@@ -11,9 +11,13 @@ struct ContentView: View {
 	
 	let apController = PredatorController()
 	@State var sortAlphabet = false
+	@State var currentFilter = "All"
 	
 	var body: some View {
-		NavigationView {
+		
+		apController.filterBy(type: currentFilter) // I want to do the same with this code, I don't want to have to write return
+		
+		return NavigationView {
 			List {
 				ForEach(sortAlphabet ? apController.sortedByAlphabet() : apController.sortedByMovieAppearance()) { predator in
 					NavigationLink(destination: PredatorDetail(predator: predator)) {
@@ -25,7 +29,9 @@ struct ContentView: View {
 			.toolbar {
 				ToolbarItem(placement: .navigationBarLeading) {
 					Button {
-						sortAlphabet.toggle()
+						withAnimation {
+							sortAlphabet.toggle()
+						}
 					} label: {
 						if sortAlphabet {
 							Image(systemName: "film")
@@ -34,6 +40,24 @@ struct ContentView: View {
 							Image(systemName: "textformat")
 								.foregroundColor(.gray)
 						}
+					}
+				}
+				ToolbarItem(placement: .navigationBarTrailing) {
+					Menu {
+						Picker("Filter", selection: $currentFilter.animation()) {
+							ForEach(apController.typeFilters, id: \.self) { type in
+								HStack {
+									Text(type)
+									
+									Spacer()
+									
+									Image(systemName: apController.typeIcon(for: type))
+								}
+							}
+						}
+					} label: {
+						Image(systemName: "slider.horizontal.3")
+							.foregroundColor(.gray)
 					}
 				}
 			}
