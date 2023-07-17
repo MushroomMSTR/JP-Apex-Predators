09e14eb2da953c89ecf49e2b6e454477677087e4
NazarStf <hworker98@gmail.com>
2023-07-17 19:25:58 +0300

    Lesson 10 - Filter Functions

--- JP Apex Predators/PredatorController.swift
+++ JP Apex Predators/PredatorController.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 class PredatorController {
+	private var allApexPredators: [ApexPredator] = []
 	var apexPredators: [ApexPredator] = []
+	let typeFilters = ["All", "Land", "Air", "Sea"]
 	
 	init () {
 		decodeApexPredatorData()
@@ -20,10 +22,31 @@ class PredatorController {
 				let data = try Data(contentsOf: url)
 				let decoder = JSONDecoder()
 				decoder.keyDecodingStrategy = .convertFromSnakeCase
-				apexPredators = try decoder.decode([ApexPredator].self, from: data)
+				allApexPredators = try decoder.decode([ApexPredator].self, from: data)
+				apexPredators = allApexPredators
 			} catch {
 				print("Error decoding JSON data: \(error)")
 			}
+		}
+	}
+	
+	func typeIcon(for type: String) -> String {
+		switch type {
+		case "All" : return "square.stack.3d.up.fill"
+		case "Land" : return "leaf.fill"
+		case "Air" : return "wind"
+		case "Sea" : return "drop.fill"
+		default : return "questionmark"
+		}
+	}
+	
+	func filterBy(type: String) {
+		switch type {
+		case "Land", "Air", "Sea" :
+			apexPredators = allApexPredators.filter {
+				$0.type == type.lowercased()
+			}
+		default : apexPredators = allApexPredators
 		}
 	}
 	
